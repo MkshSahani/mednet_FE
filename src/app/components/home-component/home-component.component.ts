adee48c203282e22ce9b836eaf35c905986ef982
MkshSahani <msco18333@gmail.com>
2024-06-17 19:14:31 +0530

    mednet started

---
+++ src/app/components/home-component/home-component.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from "@angular/core";
+
+
+@Component({
+    selector: 'app-home-comp',
+    templateUrl: './home-component.component.html',
+    styleUrls: ['./home-component.component.css']
+})
+export class HomeComponent implements OnInit{
+
+    constructor() {
+
+    }
+
+    ngOnInit(): void {
+        
+    }
+
+
+}
